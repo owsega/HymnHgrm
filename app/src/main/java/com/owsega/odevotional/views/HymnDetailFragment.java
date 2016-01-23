fccdef855a94a2a30b9fd9e206754cbd1e93ade2
Seyi <sowoeye@gidimobile.com>
2016-01-23 19:18:12 +0100

    add backgrounds to lyrics view, allow searching by hymn numbers

--- app/src/main/java/com/owsega/odevotional/views/HymnDetailFragment.java
+++ app/src/main/java/com/owsega/odevotional/views/HymnDetailFragment.java
@@ -4,9 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.res.ResourcesCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.owsega.odevotional.R;
@@ -25,6 +28,7 @@ public class HymnDetailFragment extends Fragment {
      * represents.
      */
     public static final String ARG_ITEM_ID = "hymn_id";
+    private final String LOG_TAG = getClass().getSimpleName();
 
     private String content = "";
     private String title = "";
@@ -61,6 +65,41 @@ public class HymnDetailFragment extends Fragment {
         View view = inflater.inflate(R.layout.hymn_detail, container, false);
         ((TextView) view.findViewById(R.id.content)).setText(content);
         ((TextView) view.findViewById(R.id.title)).setText(title);
+
+        setBackgroundImages(view);
+        hideNoHymnTextView();
+
         return view;
+    }
+
+    private void setBackgroundImages(View rootView) {
+        int random = (int) (Math.random() * 3);
+        int[] backgrounds = new int[]{
+                R.drawable.bg_1,
+                R.drawable.bg_2,
+                R.drawable.bg_3
+        };
+
+        //set the top background for single-pane mode
+        CollapsingToolbarLayout layout = (CollapsingToolbarLayout) getActivity()
+                .findViewById(R.id.toolbar_layout);
+        if (layout != null)
+            layout.setBackgroundDrawable(ResourcesCompat.getDrawable(
+                    getResources(), backgrounds[random], null));
+
+        // set the background for lyrics pane
+        ImageView background = (ImageView) rootView.findViewById(R.id.img_bg);
+        background.setBackgroundDrawable(ResourcesCompat.getDrawable(
+                getResources(), backgrounds[random], null));
+    }
+
+    private void hideNoHymnTextView() {
+        try {
+            TextView noHymn = (TextView) getActivity().findViewById(R.id.no_hymn_yet);
+            if (noHymn != null)
+                noHymn.setVisibility(View.GONE);
+        } catch (Exception e) {
+            Log.w(LOG_TAG, "Failed to hide no_hymn_yet text view");
+        }
     }
 }
